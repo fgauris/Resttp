16e4b9deba699c53ea259927adda6d8f5c62fda6
Aurimas <fgauris@gmail.com>
2015-02-11 23:42:17 +0200

    init

---
+++ src/HttpServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:61111";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Resttp started.");
+                Console.ReadKey();
+                Console.WriteLine("Resttp ended.");
+            }
+        }
+
+
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx => {
+                return ctx.Response.WriteAsync("Labas");
+            });
+
+
+
+        }
+    }
+}
